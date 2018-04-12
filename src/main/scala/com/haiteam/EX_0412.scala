48fcfc236ba0b009d6a47559108d0e729b4ccaa5
SMART-19 <dobull@naver.com>
2018-04-12 16:24:19 +0900

    ex0412

---
+++ src/main/scala/com/haiteam/EX_0412.scala
@@ -0,0 +1,47 @@
+package com.haiteam
+
+import org.apache.spark.sql.SparkSession
+
+object EX_0412 {
+  def main(args: Array[String]): Unit = {
+    val spark =SparkSession.builder().appName("hkproject").appName("hkProject").
+      config("spark.master", "local").
+      getOrCreate()
+
+    var staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
+    var staticUser = "kopo"
+    var staticPw = "kopo"
+    var selloutDb1 = "kopo_channel_seasonality_new"
+    var selloutDb2 = "kopo_product_mst"
+
+    val selloutData1FromOracle= spark.read.format("jdbc").
+      options(Map("url" -> staticUrl,"dbtable" -> selloutDb1,"user" -> staticUser, "password" -> staticPw)).load
+
+    val selloutData2FromOracle= spark.read.format("jdbc").
+      options(Map("url" -> staticUrl,"dbtable" -> selloutDb2,"user" -> staticUser, "password" -> staticPw)).load
+
+    selloutData1FromOracle.createOrReplaceTempView("selloutTable1")
+    selloutData2FromOracle.createOrReplaceTempView("selloutTable2")
+    selloutData1FromOracle.show()
+    selloutData2FromOracle.show()
+
+    var middleResult = spark.sql("select " +
+    "concat(a.regionid,'_',a.product) as keycol, " +
+    "a.regionid, " +
+      "a.product, " +
+      "a.yearweek, " +
+      "cast(a.qty as double) as qty, " +
+      "b.product_name " +
+      "from selloutTable1 a " +
+      "left join selloutTable2 b " +
+      "on a.product = b.product_id")
+
+
+
+
+
+
+
+  }
+}
+
